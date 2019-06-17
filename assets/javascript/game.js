4caf2cb8c9bab36627faa0215ac50ca3c5eab4b7
Stephanie Romanowski <stephanie.romanowski@vantage-cg.com>
2019-06-17 08:40:18 -0400

    added dash lines for each letter the user needs to guess, got the letters to write in the dashes when correctly guessed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,16 +5,64 @@ var maxGuesses = 6;
 var won = 0;
 var loss = 0;
 var index = 0;
+var stateOfHangman = 0; 
+//state of hangman tracking how many letters incorrectly guess, if 6 hangman is built
+var hiddenName; 
+window.onload = function(event){
+    initialize ();  
+}
+
+
+
+
+function initialize(){
+    var i;
+    hiddenName = ""
+        for (i = 0; i < names[index].length; i++) {
+            hiddenName = hiddenName + "_";
+        }
+document.getElementById("NameGuess").innerHTML = hiddenName;
+}
 
 document.onkeyup = function (event) {
 
     currentGuess = event.key;
-    alert(currentGuess)
+    
+ //   alert(currentGuess)
     console.log(names[index].includes(currentGuess))
+ 
+    var i;
+   // hiddenName = ""
+        for (i = 0; i < names[index].length; i++) {
+            
+            
+            if (names[index].includes(currentGuess)){
 
-    if (names[index].includes(currentGuess)) {
-        alert(names[index].indexOf(currentGuess))
-    }
+                var currentName = names[index];
+                for (var indexOfName = 0; indexOfName < currentName.length; indexOfName++){
+                    if (currentName[indexOfName] === currentGuess){
+                        hiddenName[indexOfName] = currentGuess; 
+                    }
+                }
+            
+                
+              //  alert(names[index].indexOf(currentGuess))
+           
+            }
+            //else {
+              //  hiddenName = hiddenName + " _ ";
+           // }     
+        }
+        document.getElementById("NameGuess").innerHTML = hiddenName;
+
+  //  if (names[index].includes(currentGuess)) {
+    //    alert(names[index].indexOf(currentGuess))
+    //}
+
+    //need to loop through each character in the name beingguessed
+        //set hiddden name to = hiddenName + "_"
+        
+
 
     
     // else {
